8bbcce1bbe7ce6f6edbd086ca4de318c854d255a
Danilo Feijo <danilo.silvafs@gmail.com>
2020-06-17 22:50:03 -0300

    Add api tests for update account

--- cypress/integration/practical/api/backend.spec.js
+++ cypress/integration/practical/api/backend.spec.js
@@ -47,19 +47,46 @@ describe('Tests at API level', () => {
     }).as('response')
 
     cy.get('@response').then(res => {
-      expect(res.status, 'verifica status code').to.be.equal(200)
+      expect(res.status).to.be.equal(200)
     })
 
     // Short option
     // cy.resetData(token)
     //   .then(res => {
     //     console.log('res reset: ', res);
-    //     expect(res, 'verifica status code com then').to.be.equal(200)
+    //     expect(res).to.be.equal(200)
     //   })
   });
 
+  it.only('Should update an account', () => {
+    const updatedName = 'Updated account name'
+
+    cy.request({
+      method: 'GET',
+      headers: { Authorization: `JWT ${token}` },
+      url: '/contas',
+      qs: {
+        nome: 'Conta para alterar'
+      }
+    }).then(res => {
+      cy.request({
+        method: 'PUT',
+        headers: { Authorization: `JWT ${token}` },
+        url: `/contas/${res.body[0].id}`,
+        body: {
+          nome: updatedName
+        }
+      }).as('response')
+    })
+
+    // cy.get('@response').its('status').should('be.equal', 200)
+    cy.get('@response').then(res => {
+      expect(res.status).to.be.equal(200)
+      expect(res.body.nome).to.be.equal(updatedName)
+    })
+  });
+
   // TODO - Tests to be developed
-  // it('Should update an account', () => {});
   // it('Should not create a duplicated account', () => {});
   // it('Should insert new transaction', () => { });
   // it('Should get balance', () => {});
