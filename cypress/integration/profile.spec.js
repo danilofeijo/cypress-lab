f2a879f4387dfd8362635d97bc7cf97ad50d67bf
Danilo Feijó <danilo.silvafs@gmail.com>
2021-07-09 16:05:27 -0300

    Added test steps and validations

--- cypress/integration/profile.spec.js
+++ cypress/integration/profile.spec.js
@@ -1,17 +1,22 @@
 /// <reference types="cypress" />
 
+import { ProfileUI } from '../page/profile';
+const profileElements = require('../page/profile/elements').ELEMENTS;
+
+let username;
+
 describe('When visit my profile page', () => {
   beforeEach(() => {
     const email = 'johndoe@cypresslab.com';
     const pass = 'Test;123';
+    username = 'johndoecypresslab';
 
     cy.login(email, pass);
-    // TODO visit profile page
-    // ProfileUI.visitProfile();
+    ProfileUI.visitProfile(username);
   });
 
   it('Should load my profile content', () => {
-    // TODO validate loaded elements
-    expect(123).to.eql(123);
+    cy.get(profileElements.imageUser).should('exist');
+    cy.get(profileElements.titleUsername).should('have.text', username);
   });
 });
