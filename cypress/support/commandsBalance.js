0c04a275b0fbb9f90a24935124d8a9be89117e6f
Danilo Feijó <danilo.silvafs@gmail.com>
2021-07-08 22:35:13 -0300

    Removed old code

--- cypress/support/commandsBalance.js
+++
@@ -1,6 +0,0 @@
-import locator from './locators';
-
-Cypress.Commands.add('visitPageHome', () => {
-  cy.get(locator.menu.option_home).click();
-  cy.url().should('eq', Cypress.config().baseUrlFront + '/');
-});
