41ce65b5d5ef81677b25572ef3c581bc6ee37e1b
Danilo Feijó <danilo.silvafs@gmail.com>
2021-07-08 23:21:21 -0300

    Moved visit page action

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -3,13 +3,16 @@
 import { LoginUI } from '../page/login';
 const globalElements = require('../page/global/elements').ELEMENTS;
 
-describe('Home page', () => {
-  it('Should load home page', () => {
-    const user = 'johndoe@cypresslab.com';
+describe('When visit login page', () => {
+  beforeEach(() => {
+    LoginUI.visitLogin();
+  });
+
+  it('Should loggin with valid credentials', () => {
+    const email = 'johndoe@cypresslab.com';
     const pass = 'Test;123';
 
-    LoginUI.visitLogin();
-    LoginUI.submitLogin(user, pass);
+    LoginUI.submitLogin(email, pass);
 
     cy.get(globalElements.headerBar)
       .should('contain', 'a', 'conduit')
