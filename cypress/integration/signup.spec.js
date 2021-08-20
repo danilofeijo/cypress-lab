f76cea931532a81b6c86a0b84f25f0d0e017817f
Danilo Feijó <danilo.silvafs@gmail.com>
2021-08-19 23:10:29 -0300

    Added new test cenario

--- cypress/integration/signup.spec.js
+++ cypress/integration/signup.spec.js
@@ -1,7 +1,19 @@
 /// <reference types="cypress" />
 
 import { SignupUI } from '../page/signup';
+const globalElements = require('../page/global/elements').ELEMENTS;
 
 describe.skip('On Sign up page', () => {
-  it('Should create a new user', () => {});
+  beforeEach(() => {
+    cy.visit('/cadastrarusuarios');
+  });
+
+  it('Should create a new user', () => {
+    cy.get('[data-testid="nome"]').type('nome bla');
+    cy.get('[data-testid="email"]').type('email bla');
+    cy.get('[data-testid="senha"]').type('senha bla');
+    cy.get('[data-testid="cadastrar"]').click();
+
+    cy.get(globalElements.butonLogout).should('exist');
+  });
 });
