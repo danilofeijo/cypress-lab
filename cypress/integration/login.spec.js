e508cd3d6133d51946e7adca542d0fecf179c8bb
Danilo Feijó <danilo.silvafs@gmail.com>
2021-08-23 22:47:26 -0300

    Fixed login test

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,19 +1,40 @@
 /// <reference types="cypress" />
 
 import { LoginUI } from '../page/login';
+import { SignupAPI } from '../page/signup';
 const globalElements = require('../page/global/elements').ELEMENTS;
+const faker = require('faker');
+
+let USER;
 
 describe('On login page', () => {
   beforeEach(() => {
+    const randonName = faker.name.findName();
+    const randomAlias = randonName.toLowerCase().replace(/\s+/g, '');
+
+    USER = {
+      nome: randonName,
+      email: `${randomAlias}@test.com`,
+      password: `Test;123`,
+      administrador: 'true',
+    };
+
+    SignupAPI.createUser(USER);
+
     LoginUI.visitLogin();
   });
 
-  it.skip('Should login with valid credentials', () => {
-    const email = Cypress.env('userName');
-    const pass = Cypress.env('userPass');
+  it('Should login with valid credentials', () => {
+    // Arrange
+    const LOGIN = {
+      EMAIL: USER.email,
+      PASS: USER.password,
+    };
 
-    LoginUI.submitLogin(email, pass);
+    // Act
+    LoginUI.submitLogin(LOGIN.EMAIL, LOGIN.PASS);
 
-    cy.get(globalElements.butonLogout).should('exist');
+    // Assert
+    cy.get(globalElements.buttonLogout).should('exist');
   });
 });
