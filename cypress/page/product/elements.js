2b84774d04576eda90104fdd9e51a22388ed3ee1
Danilo Feijó <danilo.silvafs@gmail.com>
2021-10-06 21:43:16 -0300

    feat: map elements on new product page

---
+++ cypress/page/product/elements.js
@@ -0,0 +1,11 @@
+// Elements to interact with Product page
+
+export const ELEMENTS = {
+  newProduct: {
+    inputName: '[data-testid="nome"]',
+    inputPrice: '[data-testid="preco"]',
+    inputDescription: '[data-testid="descricao"]',
+    inputQuantity: '[data-testid="quantidade"]',
+    buttonSave: '[data-testid="cadastarProdutos"]',
+  },
+};
