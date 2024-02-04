8c4030e5d0112cd7cda64a3bc44dc9f45cd1e085
Danilo Feijó <danilo.silvafs@gmail.com>
2024-02-04 16:44:19 -0300

    refactor: decrease complexity of another scenario

--- cypress/page/product/index.js
+++ cypress/page/product/index.js
@@ -1,19 +1,10 @@
 // Actions to interact with Product page
 
-const pageElements = require('./elements').ELEMENTS;
+// Will be needed when create the first UI action
+// const elementsProductCreate = require('./elements').ELEMENTS;
 
 class ProductUI {
-  submitNewProduct(productData) {
-    cy.get(pageElements.newProduct.inputName).type(productData.NAME);
-    cy.get(pageElements.newProduct.inputPrice).type(productData.PRICE);
-    cy.get(pageElements.newProduct.inputDescription).type(productData.DESCRIPTION);
-    cy.get(pageElements.newProduct.inputQuantity).type(productData.QUANTITY);
-    // Cypress image upload looks good. However...
-    // Applications image upload doesn't seem to work.
-    // So I kept image upload feature anyway.
-    cy.get(pageElements.newProduct.inputImageUpload).selectFile('cypress/fixtures/miamiGuardHouse.png');
-    cy.get(pageElements.newProduct.buttonSave).click();
-  }
+  // Space to create UI actions when needed
 }
 
 module.exports = {
