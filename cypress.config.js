0106cf92361a2a029b5aa7c5ca2db5dbd9699122
Danilo Feijó <danilo.silvafs@gmail.com>
2024-02-10 19:08:13 -0300

    ci: set parallel config 2

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,14 @@ const { defineConfig } = require('cypress');
 const cypressSplit = require('cypress-split');
 
 module.exports = defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    reportFilename: '[name].html',
+    overwrite: true,
+    html: true,
+    json: false,
+  },
   // setupNodeEvents can be defined in either
   // the e2e or component configuration
   e2e: {
@@ -14,8 +22,8 @@ module.exports = defineConfig({
       config.viewportHeight = 788;
       config.video = true;
       config.defaultCommandTimeout = 10000;
-      config.reporter = 'cypress-multi-reporters';
-      config.reporterOptions = { configFile: 'reporter-config.json' };
+      // config.reporter = 'cypress-multi-reporters';
+      // config.reporterOptions = { configFile: 'reporter-config.json' };
 
       const environmentName = config.env.environmentName || 'qa';
       const environmentFilename = `./config/${environmentName}.json`;
