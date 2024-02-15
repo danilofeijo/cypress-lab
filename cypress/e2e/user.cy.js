f0bb3e313ad31ee855a55e34cf1b847e578bd8a7
Danilo Feijó <danilo.silvafs@gmail.com>
2024-02-15 17:37:21 -0300

    test: map tests to develop

---
+++ cypress/e2e/user.cy.js
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+
+describe.skip('As Admin user', () => {
+  describe('On create user page', () => {
+    it('Should create a new common user', () => {
+      // TODO - Develop test
+      // Arrange
+      // Act
+      // Assert
+    });
+    it('Should create a new admin user', () => {
+      // TODO - Develop test
+      // Arrange
+      // Act
+      // Assert
+    });
+  });
+
+  describe('On list user page', () => {
+    it('Should delete a common user', () => {
+      // TODO - Develop test
+      // Arrange
+      // Act
+      // Assert
+    });
+    it('Should delete a admin user', () => {
+      // TODO - Develop test
+      // Arrange
+      // Act
+      // Assert
+    });
+  });
+});
